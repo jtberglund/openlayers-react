17a33c0e820750f708469a148b4cdcf8d3ef5e56
Jeremy Berglund <jtberglund@gmail.com>
2019-06-04 18:24:01 -0700

    Got example page running

--- examples/simple/gulpfile.js
+++ examples/simple/gulpfile.js
@@ -2,54 +2,78 @@ const gulp = require('gulp');
 const liveServer = require('live-server');
 const rollup = require('rollup');
 const babel = require('rollup-plugin-babel');
-const resolve = require('rollup-plugin-node-resolve');
+const nodeResolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 const postcss = require('rollup-plugin-postcss');
 
-function serve() {
-    liveServer.start({
-        file: 'public/index.html'
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+const inputOptions = {
+    input: 'src/index.tsx',
+
+    // Tell Rollup NOT to bundle React/ReactDOM with our app
+    // We'll grab these libraries from a CDN
+    external: ['react', 'react-dom'],
+
+    plugins: [
+        nodeResolve({
+            extensions
+        }),
+        commonjs({
+            include: 'node_modules/**'
+        }),
+        postcss({
+            extract: `lib/bundle.css`,
+            extensions: ['.css'],
+            plugins: []
+        }),
+        babel({
+            exclude: 'node_modules/**',
+            extensions
+        })
+    ]
+};
+
+const outputOptions = {
+    file: 'lib/bundle.js',
+    format: 'iife',
+    sourcemap: true,
+
+    // Names of the global objects for external libraries
+    globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+    }
+};
+
+async function build() {
+    const bundle = await rollup.rollup(inputOptions);
+
+    // write the bundle to disk
+    await bundle.write(outputOptions);
+}
+
+async function watch() {
+    await rollup.watch({
+        ...inputOptions,
+        output: [outputOptions],
+        watch: {
+            include: 'src/**',
+            exclude: 'src/**/*.test.*'
+        }
     });
 }
 
-async function build() {
-    const extensions = ['.js', '.jsx', '.ts', '.tsx'];
-    const bundle = await rollup.rollup({
-        input: 'src/index.tsx',
-        plugins: [
-            resolve({ extensions }),
-            commonjs({
-                include: 'node_modules/**',
-                exclude: '**/*.css',
-                // left-hand side can be an absolute path, a path
-                // relative to the current directory, or the name
-                // of a module in node_modules
-                namedExports: {
-                    'node_modules/react/index.js': ['cloneElement', 'createContext', 'Component', 'createElement'],
-                    'node_modules/react-dom/index.js': ['render', 'hydrate']
-                }
-            }),
-            postcss({
-                extract: `lib/bundle.css`,
-                extensions: ['.css'],
-                plugins: []
-            }),
-            babel({
-                exclude: 'node_modules/**',
-                extensions
-            })
-        ]
-    });
-    // console.log(bundle.watchFiles); // an array of file names this bundle depends on
-
-    // write the bundle to disk
-    await bundle.write({
-        file: 'lib/bundle.js',
-        format: 'cjs'
+function serve() {
+    liveServer.start({
+        file: 'public/index.html',
+        // Reload the browser when the bundle changes
+        watch: ['lib']
     });
 }
 
 gulp.task(serve);
 gulp.task(build);
+gulp.task(watch);
 
-exports.default = gulp.parallel(build, serve);
+exports.default = gulp.parallel(watch, serve);
--- examples/simple/src/App.tsx
+++ examples/simple/src/App.tsx
@@ -4,11 +4,14 @@ import { Map } from 'openlayers-react';
 import React from 'react';
 
 const App: React.FC = () => {
+    React.useEffect(() => {
+        console.log('mounted');
+    }, []);
     return (
         <div className="App">
             <header className="App-header">
                 <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
+                    Edit <code>src/App.tsx</code> and save to reload
                 </p>
                 <Map />
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
